5ed414614080fd3a8af7491db7f80da41023f0a6
Areesha <154413228+ariiwhattheheck@users.noreply.github.com>
2023-12-27 18:38:14 +0500

    Create age (ahmawd prg).cpp

---
+++ age (ahmawd prg).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout << "Enter your age:";
+    cin >> age;
+
+    if (age > 18)
+        cout << "KILL YOURSELF" <<endl;
+    else
+        cout << "Good Night" <<endl;
+    return 0;
+}
