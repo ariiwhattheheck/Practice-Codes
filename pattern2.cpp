96579e7d16a0aa8297f5e5423f8858d73d25a3d2
Areesha <154413228+ariiwhattheheck@users.noreply.github.com>
2024-02-17 06:25:40 +0500

    Create pattern2.cpp

---
+++ pattern2.cpp
@@ -0,0 +1,26 @@
+/* *****
+    ****
+     ***
+      **
+       * */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter number:";
+    cin >> num;
+
+    for (int r=num; r>=1;r--)
+    {
+        for (int c=r;c>0;c--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
